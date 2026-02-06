12a47902824fca8941dfd3546ff04a49b1a9cec8
Michael McCain <michaelmc1989@gmail.com>
2026-02-06 08:17:08 -0500

    refactored file read

--- stable-match/assignment-1.py
+++ stable-match/assignment-1.py
@@ -3,36 +3,50 @@ from pathlib import Path
 from operator import itemgetter
 import itertools
 
-### Variables ### 
-# Path Variables
-in_file = Path(__file__).parent / 'Input.txt'
-out_path = Path(__file__).parent
-
-# Other variables
-name_dict = {} # Intialized list of all names
-proposer_dict = {} # Intialized list of propser preference
-proposee_dict = {}  # Initilaized list of propsee preference
-unmatched_proposers = [] # Initlaized List of matched propsers 
-
-### MAIN ### 
-## Read in file to populate dictionaries and lists
-line_count = 0
-with open(in_file, 'r') as file:
-    next(file)
-    lines = file.read()
-    names = lines.strip().split('\n')
+### Functions ###
+# Read input file, return dictionary and integer from first line 
+def read_file(in_file):
+    final_dict = {}
+    with open(in_file, 'r') as file:
+        match_int = file.readline().strip()
+        lines = file.read()
+        names = lines.strip().split('\n')
 
     for name in names: # Populate name dictionary
         x = name.split(' ')
         propser = itemgetter(0)
-        name_dict.update({propser(x) : x[1:(len(x))]})
+        final_dict.update({propser(x) : x[1:(len(x))]})
 
-line_count = (len(x) - 1)
-proposer_dict = dict(itertools.islice(name_dict.items(), 0, line_count)) # populate proposer dictionary (Men)
-proposee_dict = dict(itertools.islice(name_dict.items(), line_count,len(name_dict))) # Populate propsee dictionary (Ladies)
+    return int(match_int), final_dict
 
-for key in proposer_dict.keys():    # Populate unmatched list for while condition
-    unmatched_proposers.append(key) 
+### MAIN ### 
+def main():
+    ### Variables ### 
+    # Path Variables
+    in_file = Path(__file__).parent / 'Input.txt'
+    out_path = Path(__file__).parent
 
-# While there is a man m who is free and hasn’t proposed to
-# every woman w for which (m, w) ~F
+    # Other variables
+    name_dict = {} # Intialized list of all names
+    proposer_dict = {} # Intialized list of propser preference
+    proposee_dict = {}  # Initilaized list of propsee preference
+    unmatched_proposers = [] # Initlaized List of matched propsers 
+    match_count = 0
+
+    # Read Input.txt to get dictionary and match count
+    match_count, name_dict = read_file(in_file)
+
+    proposer_dict = dict(itertools.islice(name_dict.items(), 0, match_count)) # populate proposer dictionary (Men)
+    proposee_dict = dict(itertools.islice(name_dict.items(), match_count,len(name_dict))) # Populate propsee dictionary (Ladies)
+
+    # print('Men: ', proposer_dict)
+    # print('Women: ', proposee_dict)
+
+    for key in proposer_dict.keys():    # Populate unmatched list for while condition
+        unmatched_proposers.append(key) 
+
+    # While there is a man m who is free and hasn’t proposed to
+    # every woman w for which (m, w) ~F
+
+if __name__ == "__main__":
+    main()
